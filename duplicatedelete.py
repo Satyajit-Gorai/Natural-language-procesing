8b52e690e0f68d6032e38b8d0e3d9b80831d43a6
Satyajit-Gorai <47174089+Satyajit-Gorai@users.noreply.github.com>
2020-03-24 23:38:58 +0530

    now it will work with case insensetive file

--- duplicatedelete.py
+++ duplicatedelete.py
@@ -1,8 +1,8 @@
 import fileinput
 #enter the file name that contains all the words that are to be eliminate from the other file.
 with open('bx.txt') as fin:
-    exclude = set(line.rstrip() for line in fin)
+    exclude = set(line.rstrip().lower() for line in fin)
 #enter the file name whose words are to be deleted.
 for line in fileinput.input('ax.txt', inplace=True):
-    if line.rstrip() not in exclude:
+    if line.rstrip().lower() not in exclude:
         print (line)
